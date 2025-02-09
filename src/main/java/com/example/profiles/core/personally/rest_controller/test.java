3aeb0d81c59b61d77021a57a1e7fb21018006cf1
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-02-09 22:25:04 +0700

    update function print PDF

--- src/main/java/com/example/profiles/core/personally/rest_controller/test.java
+++
@@ -1,7 +0,0 @@
-package com.example.profiles.core.personally.rest_controller;
-
-public class test {
-    public static void main(String[] args) {
-        String a = "\"info,career-goals,skills,education,experience,project,more-information\"";
-    }
-}
